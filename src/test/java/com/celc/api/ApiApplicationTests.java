40b9f7be492ca0475a6d1553b935742ec8fddcd7
RhuanLino <rhuanlino309@gmail.com>
2025-04-01 16:10:36 -0300

    first commit

---
+++ src/test/java/com/celc/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celc.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
